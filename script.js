a317c6dd7b58b85d1b2a57626d35a46a6966cf8d
John Yong <43509228+jessumguy@users.noreply.github.com>
2023-11-24 17:55:15 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    }
+}
+
+function addBookToLibrary() {
+    
+}
+
+
+let theHobbit =  new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
+console.log(theHobbit.info())
