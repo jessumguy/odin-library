c38efa508a7e44a2dbf17201ef9970c107ce9808
John Yong <43509228+jessumguy@users.noreply.github.com>
2023-12-08 18:34:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let initialPageRender = false
+let rowNo = 0
+
+
 // Books stored in myLibrary Array
 
-const myLibrary = [
+let myLibrary = [
     { 
         title: "The Hobbit",
         author: "J.R.R Tolkien",
@@ -18,10 +22,11 @@ const bookTitle = document.querySelector("#book_title");
 const bookAuthor = document.querySelector("#book_author");
 const bookPages = document.querySelector("#book_pages");
 const bookReadStatus = document.querySelector("#book_read");
+
 const addNewBookButton = document.querySelector("#add_new_book_button").addEventListener("click", (e) => {
     addBookToLibrary();
     renderPage();
-    console.log('Add New Book')
+    console.log("Added New Book!")
 });
 
 // Book Object
@@ -33,32 +38,71 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-// Creates new Book Object and adds it to myLibrary Array 
+// Creates new Book Object and adds it to myLibrary Array
 
 function addBookToLibrary() {
     const newBook = new Book(`${bookTitle.value}`, `${bookAuthor.value}`, `${bookPages.value}`, `${bookReadStatus.value}`)
     myLibrary.push(newBook)
 }
 
+// Remove row from table
+
+function deleteRow(rowNo) {
+    const tableRow = document.getElementById(rowNo);
+    tableRow.parentNode.removeChild(tableRow);
+    console.log("Deleted Book from Row!")
+    
+    removeBookFromMyLibraryArray();
+}
+
+// Remove Book from myLibrary Array https://stackoverflow.com/questions/66728040/how-to-associate-object-with-dom-element-in-plain-javascript
+
+function removeBookFromMyLibraryArray(i) {
+    myLibrary.splice(myLibrary.indexOf(myLibrary[i]), 1);
+    console.log(myLibrary);
+    console.log(i)
+}
+
 // Renders the Page
 
 function renderPage() {
-    bookshelfTable.innerHTML = "";
+    // bookshelfTable.innerHTML = "";
 
-    myLibrary.forEach((book) => {
-        const bookHTML = 
+    if (initialPageRender === false) {
+        myLibrary.forEach((book) => {
+            const defaultBooks = 
+            `
+                <tr id="row-${rowNo}">
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.pages}</td>
+                    <td>${book.read}</td>
+                    <td><button id="edit-book-row">Edit</button></td>
+                    <td><button id="delete-book-row-${rowNo}" onclick="deleteRow('row-${rowNo}')">Delete</button></td>
+                </tr>
+            `;
+            bookshelfTable.insertAdjacentHTML("beforeend", defaultBooks);
+
+            initialPageRender = true
+            rowNo++
+        });
+
+    } else {
+        const newBookToBeAdded = 
         `
-            <tr>
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.pages}</td>
-                <td>${book.read}</td>
+            <tr id="row-${rowNo}">
+                <td>${bookTitle.value}</td>
+                <td>${bookAuthor.value}</td>
+                <td>${bookPages.value}</td>
+                <td>${bookReadStatus.value}</td>
                 <td><button id="edit-book-row">Edit</button></td>
-                <td><button id="delete-book-row">Delete</button></td>
+                <td><button id="delete-book-row-${rowNo}" onclick="deleteRow('row-${rowNo}')">Delete</button></td>
             </tr>
         `;
-        bookshelfTable.insertAdjacentHTML("beforeend", bookHTML);
-    });
+        bookshelfTable.insertAdjacentHTML("beforeend", newBookToBeAdded);
+
+        rowNo++
+    }
 }
 
 renderPage();
